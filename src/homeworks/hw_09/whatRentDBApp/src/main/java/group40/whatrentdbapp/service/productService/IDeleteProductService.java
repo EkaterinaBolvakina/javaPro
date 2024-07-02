6ea5a7a575f4e1e2e1574816a80aff1603a1b26f
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-07-02 20:28:43 +0200

    HW lesson 09 Project Spring Boot Data

--- src/homeworks/hw_09/whatRentDBApp/src/main/java/group40/whatrentdbapp/service/productService/IDeleteProductService.java
+++ src/homeworks/hw_09/whatRentDBApp/src/main/java/group40/whatrentdbapp/service/productService/IDeleteProductService.java
@@ -1,7 +1,5 @@
 package group40.whatrentdbapp.service.productService;
 
-import group40.whatrentdbapp.dto.RequestProductDto;
-import group40.whatrentdbapp.entity.Product;
 import org.springframework.http.ResponseEntity;
 
 public interface IDeleteProductService {
